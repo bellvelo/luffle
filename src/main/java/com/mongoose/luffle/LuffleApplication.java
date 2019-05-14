058ef1cda7c3e005450a07b076425b84487866dc
David Bell <dbelldev@icloud.com>
2019-05-14 20:10:43 +0100

    initial commit driver issues

---
+++ src/main/java/com/mongoose/luffle/LuffleApplication.java
@@ -0,0 +1,13 @@
+package com.mongoose.luffle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuffleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuffleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mongoose/luffle/LuffleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mongoose.luffle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LuffleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
